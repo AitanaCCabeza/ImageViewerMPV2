82828001925305a701d701cfdd977e5e63f3df3e
aitan <aitan@LAPTOP-5TR53I5R>
2021-01-18 14:33:42 +0000

    ImageViewerMPV2

---
+++ src/imageviewermvp2/model/Image.java
@@ -0,0 +1,15 @@
+package imageviewermvp2.model;
+
+public class Image {
+    private final String name;
+        
+    public Image(String name){
+        this.name = name;
+    }
+        
+    public String getName(){
+        return name;
+    }
+    
+    
+}
